f501bea919f838fa392d777034bf18e164768513
Maxime Lardenois <maxime.lardenois@gmail.com>
2022-12-29 15:42:51 +0100

    feat:Add sign-in route and component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sigin-in/sign-in.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />}>
-        </Route>
+        <Route index element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,6 +15,9 @@ const Navigation = () => {
           <Link className="nav-link" to='/shop'>
             Shop
           </Link>
+          <Link className="nav-link" to='/sign-in'>
+            SignIn
+          </Link>
         </div>
       </nav>
       <Outlet />
