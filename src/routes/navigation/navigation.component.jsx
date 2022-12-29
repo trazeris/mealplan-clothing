cb632e6cfa53eeff9576b4b77f79774fccf5afb3
Maxime Lardenois <maxime.lardenois@gmail.com>
2022-12-29 15:28:02 +0100

    feat:Add routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <nav className="navigation">
+        <Link className="logo-container" to='/'>
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>
+            Shop
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
