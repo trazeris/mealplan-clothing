599c13f4c52301481294c49da5be675df6906dc2
Maxime Lardenois <maxime.lardenois@gmail.com>
2023-01-02 21:12:53 +0100

    feat:Add shopping icon and cart scaffolding

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,12 +2,16 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import { CartContext } from "../../contexts/cart.context";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -21,12 +25,16 @@ const Navigation = () => {
           </Link>
           { 
             currentUser ? 
-            <span className="nav-link" onClick={signOutUser}>Sign out</span> : 
+            (
+              <span className="nav-link" onClick={signOutUser}>Sign out</span>
+              ) : 
             <Link className="nav-link" to='/auth'>
               SignIn
             </Link>
           }
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </nav>
       <Outlet />
     </Fragment>
