2844c0831b1e6ba3fd02f0e594ffbf753c37007c
Maxime Lardenois <maxime.lardenois@gmail.com>
2023-01-15 20:25:24 +0100

    feat:Implement lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import Authentication from './routes/authentication/authentication.component';
-import Checkout from './routes/checkout/checkout.component';
-import Home from './routes/home/home.component';
+
+import { GlobalStyle } from './global.styles';
+
+import Spinner from './components/spinner/spinner.component';
+
 import Navigation from './routes/navigation/navigation.component';
-import Shop from './routes/shop/shop.component';
 import { checkUserSession } from './store/user/user.action';
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Authentication = lazy(
+  () => import('./routes/authentication/authentication.component')
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,14 +24,19 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />}></Route>
-        <Route path="auth" element={<Authentication />}></Route>
-        <Route path="shop/*" element={<Shop />}></Route>
-        <Route path="checkout" element={<Checkout />}></Route>
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />}></Route>
+            <Route path="auth" element={<Authentication />}></Route>
+            <Route path="shop/*" element={<Shop />}></Route>
+            <Route path="checkout" element={<Checkout />}></Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-import './index.scss';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
